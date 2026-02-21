b590cd359597753556cdf531620c29b78d9eb60a
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2026-02-21 14:53:12 +0100

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -51,7 +51,7 @@ app.use('/scrim-session', require('./controllers/scrim-session'));
 app.use('/enemy-team', require('./controllers/enemy-team'));
 app.use('/player', require('./controllers/player'));
 app.use('/soloq-snapshot', require('./controllers/soloQ-snapshot'));
-app.use('/soloq-match', require('./controllers/soloq-match'));
+app.use('/soloq-match', require('./controllers/soloQ-match'));
 setupErrorHandler(app);
 require('./services/passport')(app);
 require('./cron');
