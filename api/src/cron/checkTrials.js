94da7f455b0831cd456b75d54a9691f437c7fbdb
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2026-04-12 12:01:46 +0200

    Update checkTrials.js

--- api/src/cron/checkTrials.js
+++ api/src/cron/checkTrials.js
@@ -1,7 +1,7 @@
 const Team = require('../models/team');
 
 module.exports = async () => {
-  const result = await Team.updateMany({ subscription_status: 'trialing', subscription_current_period_end: { $lt: new Date() } }, { subscription_status: 'canceled', subscription_current_period_end: null });
+  const result = await Team.updateMany({ subscription_status: 'trialing', subscription_current_period_end: { $lt: new Date() } }, { subscription_status: 'canceled' });
   if (result.modifiedCount > 0) {
     console.log(`Expired ${result.modifiedCount} trial(s)`);
   }
