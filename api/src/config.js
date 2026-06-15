3568a2ec388995ee87e7245b6f161a8a83973b98
axelvuillaume <105872552+axelvuillaume@users.noreply.github.com>
2026-06-15 13:00:06 +0200

    Update config.js

--- api/src/config.js
+++ api/src/config.js
@@ -5,7 +5,7 @@ const MONGODB_ENDPOINT = 'mongodb+srv://axelvuillaume:gtb5r5m0CSkOdC5o@cluster0.
 const SECRET = process.env.SECRET || 'not-so-secret';
 const APP_URL = process.env.APP_URL || 'http://localhost:3000';
 const CLAUDE_API_KEY = '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';
-const GEMINI_API_KEY = 'RGAPI-1f9bef35-31aa-4061-975c-d503fd351f22';
+const GEMINI_API_KEY = 'AIzaSyDwE9dY83XGkuOsp-Wafqf-5KbjnUTyoog';
 const SENTRY_DSN = process.env.SENTRY_DSN || '';
 
 const S3_ENDPOINT = process.env.S3_ENDPOINT || '';
@@ -14,7 +14,7 @@ const S3_SECRETACCESSKEY = process.env.S3_SECRETACCESSKEY || '';
 
 const BREVO_KEY = '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';
 
-const RIOT_API_KEY = 'RGAPI-14f41352-b408-4b00-b711-67eb974b9ec9';
+const RIOT_API_KEY = 'RGAPI-1f9bef35-31aa-4061-975c-d503fd351f22';
 
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '';
 const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || '';
